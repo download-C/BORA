8a183fe344e038c5c7dafbe6c12f98593701c2e4
download-C <104185416+download-C@users.noreply.github.com>
2022-11-22 19:10:37 +0900

    [update] 공지사항 첨부파일 경로 수정

--- src/main/java/com/bora/controller/board/NoticeController.java
+++ src/main/java/com/bora/controller/board/NoticeController.java
@@ -68,7 +68,7 @@ public class NoticeController {
 			
 			String fileExtension = ofileName.substring(ofileName.lastIndexOf("."),ofileName.length());
 			// 저장 위치
-			String uploadFolder = "C:\\Users\\ITWILL\\git\\BORA\\src\\main\\webapp\\resources\\upload";
+			String uploadFolder = "../tomcat/bin/upload";
 			
 			log.info("원래 파일명: "+ofileName);
 			// 업로드될 파일 이름들 저장
